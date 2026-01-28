8bcc2c4be475e510b4ee0fe64be2f01e6495c690
Pixel4Inch <77658911+pixel4inch@users.noreply.github.com>
2026-01-28 12:18:10 +0530

    vite.config updated

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   preview: {
-    allowedHosts: ['sstzigane.onrender.com']
+    allowedHosts: ['.onrender.com']
   }
 })
